ee85ef1e7adacd7797cc76d979483d8b739467b5
MG-Developers <92076608+MG-Developers@users.noreply.github.com>
2024-01-25 00:14:33 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,18 +1,57 @@
-$(document).ready(function() {
-  $.ajax({
-    url: "https://jsonplaceholder.typicode.com/users",
-    method: "GET",
-    success: function(data) {
-      $("#myDataTable").DataTable({
-        data: data,
-        columns: [{ data: "id" }, { data: "name" }, { data: "email" }],
-        pageLength: 5,
-        lengthMenu: [10, 25, 50, 75, 100],
-        pagingType: "full_numbers",
-      });
+$(document).ready(function() 
+{
+  const token = JSON.parse(localStorage.getItem("token"));
+  // console.log(token);
+  var totalPages = 10;
+  var defaultPageSize = 10;
+
+  var table = $("#myDataTable").DataTable({
+    processing: true,
+    serverSide: true,
+
+    ajax: {
+      url: function() {
+        var page = dataTable.page.info().page + 1;
+        var pageSize = dataTable.page.info().length;
+        return "http://192.168.50.81:8080/ap_automation_backend/usermaster/users?page=" + page + "&size=" + pageSize;
+      },
+      type: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      dataSrc: function(json) {
+        totalPages = json.totalPages;
+        defaultPageSize = json.defaultPageSize;
+
+        return json.data;
+      },
     },
-    error: function(error) {
-      console.log("Error: ", error);
-    },
+
+    columns: [
+      { data: "userid" },
+      { data: "username" },
+      { data: "address_number" },
+      { data: "email" },
+      {
+        data: "userid",
+        render: function(data, type, row, meta) {
+          return `
+                    <div class="btn-group">
+                        <button class='btn btn-outline-danger btn-sm delete'>Delete</button>&nbsp;&nbsp;  
+                        <button class='btn btn-outline-success btn-sm edit'>Edit</button>&nbsp;&nbsp;
+                        <button class='btn btn-outline-success btn-sm view'>View</button>
+                    </div>
+                `;
+        },
+      },
+    ],
+
+    // columnDefs: [
+    //   { targets: 0, visible: false },
+    // ],
+
+    pageLength: defaultPageSize,
+    lengthMenu: [10, 25, 50, 75, 100],
+    
   });
 });
