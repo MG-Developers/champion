aa7d7535cf800955a2bb48b77a3bc9deb7555642
MG-Developers <92076608+MG-Developers@users.noreply.github.com>
2025-05-12 18:38:23 +0530

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,24 +1,123 @@
-import { HeroSection } from "@/app/sections/HeroSection"
-import { ServiceSection } from "@/app/sections/ServiceSection"
-import { TreatmentSection } from "@/app/sections/TreatmentSection"
-import { AmenitySection } from "@/app/sections/AmenitySection"
-import { TestimonialSection } from "@/app/sections/TestimonialSection"
-import { Newsletter } from "@/app/components/newsletter"
-import BrochureSection from "./sections/BrochureSection"
-
+"use client"
+import { AnimatedText } from "@/app/components/AnimatedText"
 import { IoIosArrowForward } from "react-icons/io";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 export default function Home() {
+
+  const priceComparisonTableData = [
+    {
+      id: "core-features",
+      name: "Core Features",
+      features: [{
+        id: "budgeting",
+        name: "Budgeting",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "track-metrics",
+        name: "Track Metrics",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "debt-management",
+        name: "Debt Management",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "saving-goals",
+        name: "Saving Goals",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      }],
+    },
+
+    {
+      id: "advance-features",
+      name: "Advance Features",
+      features: [{
+        id: "budgeting",
+        name: "Budgeting",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "track-metrics",
+        name: "Track Metrics",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "debt-management",
+        name: "Debt Management",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "saving-goals",
+        name: "Saving Goals",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      }],
+    },
+
+    {
+      id: "additional-benefits",
+      name: "Additional Benefits",
+      features: [{
+        id: "budgeting",
+        name: "Budgeting",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "track-metrics",
+        name: "Track Metrics",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "debt-management",
+        name: "Debt Management",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      },
+
+      {
+        id: "saving-goals",
+        name: "Saving Goals",
+        isStarter: true,
+        isProfessional: true,
+        isEnterprise: true
+      }],
+    },
+  ]
+
   return (
     <>
       <main>
-        {/* <HeroSection /> */}
-        {/* <BrochureSection />
-        <ServiceSection />
-        <TreatmentSection />
-        <AmenitySection />
-        <TestimonialSection />
-        <Newsletter /> */}
         <section>
           <div className="h-screen">
             <div className="w-9/12 mx-auto h-screen flex flex-col space-y-5 items-center justify-center">
@@ -36,6 +135,20 @@ export default function Home() {
 
               <div>
                 <button className="font-medium flex items-center gap-2">Check all services <span><IoIosArrowForward /></span></button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="h-screen flex items-center">
+            <div className="w-11/12 mx-auto">
+              <div className="grid grid-cols-2">
+                <div>
+                  <AnimatedText text="A marketing website is a complex thing to build on your own" />
+                </div>
+
+                <div></div>
               </div>
             </div>
           </div>
@@ -95,7 +208,80 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="h-screen"></section>
+        <section className="w-11/12 mx-auto py-24">
+          <div className="flex flex-col tracking-wide">
+            <div className="flex flex-col items-center text-center space-y-6">
+              <h2 className="xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-medium">Plan Comparison</h2>
+              <p className="text-base max-w-md text-gray-600">Explore detailed answers to the most commonly asked questions about our platform, features, integrations</p>
+            </div>
+
+            <div className="mt-6 py-10 border-b border-gray-300">
+              <div className="grid grid-cols-4 gap-8 items-center">
+                <div>
+                  <p>Perfect for individuals or small teams</p>
+                  <p>Monthly</p>
+                </div>
+
+                <div className="flex flex-col justify-between h-full">
+                  <div>
+                    <h3 className="text-2xl font-medium py-4">Starter</h3>
+                    <p className="text-sm mb-4 text-gray-600 font-medium">0$/month, billed monthly. Perfect for individuals or small teams</p>
+                  </div>
+                  <button className="border border-gray-100 bg-gray-100 rounded-lg w-full py-3 px-6 text-sm font-medium">Get Started Now</button>
+                </div>
+
+                <div className="flex flex-col justify-between h-full">
+                  <div>
+                    <h3 className="text-2xl font-medium py-4">Professional</h3>
+                    <p className="text-sm mb-4 text-gray-600 font-medium">12,99$/month, billed monthly. Perfect for scaling company, big corporate</p>
+                  </div>
+                  <button className="border border-black bg-black text-white rounded-lg w-full py-3 px-6 text-sm font-medium">Get Started Now</button>
+                </div>
+
+                <div className="flex flex-col justify-between h-full">
+                  <div>
+                    <h3 className="text-2xl font-medium py-4">Enterprise</h3>
+                    <p className="text-sm mb-4 text-gray-600 font-medium">Contact us for custom pricing and billing terms</p>
+                  </div>
+                  <button className="border border-gray-100 bg-gray-100 rounded-lg w-full py-3 px-6 text-sm font-medium">Get Started Now</button>
+                </div>
+              </div>
+            </div>
+
+            {priceComparisonTableData.map((row, index) => {
+              return (
+                <div key={index} className="grid grid-flow-row auto-rows-fr">
+                  <div className="mt-10">
+                    <h2 className="text-xl font-medium py-4">{row.name}</h2>
+                    {row.features.map((feature, index) => {
+                      return (
+                        <div key={index} className="grid grid-cols-4 gap-8 items-center py-6 border-b border-gray-200">
+                          <div>
+                            <p className="text-base font-medium text-gray-600">{feature.name}</p>
+                          </div>
+
+                          <div className="flex flex-col items-center">
+                            <p>{feature.isStarter ? <IoIosCheckmarkCircle className="text-black" size={24} /> : <></>}</p>
+                          </div>
+
+                          <div className="flex flex-col items-center">
+                            <p>{feature.isProfessional ? <IoIosCheckmarkCircle className="text-black" size={24} /> : <></>}</p>
+                          </div>
+
+                          <div className="flex flex-col items-center">
+                            <p>{feature.isEnterprise ? <IoIosCheckmarkCircle className="text-black" size={24} /> : <></>}</p>
+                          </div>
+                        </div>
+                      )
+                    })}
+
+                  </div>
+                </div>
+              )
+            })}
+
+          </div>
+        </section>
       </main>
     </>
   )
